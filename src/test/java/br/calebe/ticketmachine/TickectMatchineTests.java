2f51ecfc35607dc9ab1d5ba0696552403dea656c
Luc Bonnet <lucbonnet10@gmail.com>
2024-10-24 08:32:06 -0300

    Criação do teste imprimir bilhete com saldo zerado

--- src/test/java/br/calebe/ticketmachine/TickectMatchineTests.java
+++ src/test/java/br/calebe/ticketmachine/TickectMatchineTests.java
@@ -1,7 +1,18 @@
 package br.calebe.ticketmachine;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class TicketMachineTests {
+import br.calebe.ticketmachine.core.TicketMachine;
+import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
 
+class TicketMachineTests {
+	@Test
+	public void imprimirBilheteComSaldoZeradoTest() {
+		TicketMachine tm = new TicketMachine(10);
+
+		Assertions.assertThrows(SaldoInsuficienteException.class, () -> {
+			tm.imprimir();
+		});
+	}
 }
